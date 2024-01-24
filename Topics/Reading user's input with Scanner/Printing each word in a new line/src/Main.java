1a94633a4c055085c90258ac4e3083c89c501c54
roblebob <robert.koppisch@gmail.com>
2024-01-24 15:34:31 +0100

    Initial commit

---
+++ Topics/Reading user's input with Scanner/Printing each word in a new line/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+        for (int i = 0; i < 5; i++) {
+            System.out.println(scanner.next());
+        }
+    }
+}
---
+++ Topics/Reading user's input with Scanner/Theory/src/Task.java
@@ -0,0 +1,8 @@
+// You can experiment here, it won’t be checked
+import java.util.Scanner;
+
+public class Task {
+  public static void main(String[] args) {
+    // put your code here
+  }
+}
